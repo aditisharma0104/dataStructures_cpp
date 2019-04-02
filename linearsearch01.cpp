8ae69da41db1afcf65166a2ab97d582408529bdf
himanshutawra001 <49098607+himanshutawra001@users.noreply.github.com>
2019-04-03 01:07:12 +0530

    Add files via upload
    Linear search using class.

---
+++ linearsearch01.cpp
@@ -0,0 +1,54 @@
+
+#include<iostream>
+using namespace std;
+
+class lsearch
+{
+    public:
+    int arr[20],a_size,s_element,i;
+    void getdata();
+    void display();
+
+};
+
+void lsearch::getdata()
+{
+    cout<<"Enter the size of the array"<<endl;
+    cin>>a_size;
+
+    cout<<"Enter the elements of the array"<<endl;
+    
+    for(i=0; i<a_size; i++)
+    {
+        cin>>arr[i];
+
+    }
+    cout<<"Enter the element to be searched"<<endl;
+    cin>>s_element;
+
+
+}
+
+void lsearch::display()
+{
+    int flag=0;
+    for(i=0; i<a_size; i++)
+    {
+        if(arr[i]==s_element)
+        {
+            cout<<"Element found at position " <<(i+1)<<endl;
+            flag++;
+        }
+    }
+    if(flag==0)
+    cout<<"Element not found"<<endl;
+}
+
+int main()
+{
+    lsearch obj;
+    obj.getdata();
+    obj.display();
+    return 0;
+}
+
